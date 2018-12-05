cc1a0af6baa634e8eac636842188e7697f3704ff
linyows <linyows@gmail.com>
2018-12-05 15:58:44 +0900

    download after user/group create

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,19 +7,6 @@ execute 'systemctl daemon-reload' do
   action :nothing
 end
 
-execute 'download and extract' do
-  cwd node['dewy']['bin']
-  command <<-CMD
-    link=$(curl -s #{node['dewy']['github_api']}/#{node['dewy']['version']} | grep browser_download_url | grep #{node['dewy']['asset']} | awk '{print $2}' | tr -d '"')
-    curl -LJOs -H 'Accept: application/octet-stream' $link
-    rm -f dewy
-    tar xvf #{node['dewy']['asset']}
-    rm -f #{node['dewy']['asset']}
-    chown #{node['dewy']['user'] == 'nobody' ? 'root' : node['dewy']['user']}:#{node['dewy']['group'] == 'nobody' ? 'root' : node['dewy']['group']} dewy
-  CMD
-  only_if { !File.exists?("#{node['dewy']['bin']}/dewy") || node['dewy']['replace'] }
-end
-
 if node['dewy']['group'] == 'nobody'
   group node['dewy']['group'] do
     system true
@@ -33,6 +20,19 @@ if node['dewy']['user'] != 'nobody'
     gid node['dewy']['group']
     comment 'Service user for dewy'
   end
+end
+
+execute 'download and extract' do
+  cwd node['dewy']['bin']
+  command <<-CMD
+    link=$(curl -s #{node['dewy']['github_api']}/#{node['dewy']['version']} | grep browser_download_url | grep #{node['dewy']['asset']} | awk '{print $2}' | tr -d '"')
+    curl -LJOs -H 'Accept: application/octet-stream' $link
+    rm -f dewy
+    tar xvf #{node['dewy']['asset']}
+    rm -f #{node['dewy']['asset']}
+    chown #{node['dewy']['user'] == 'nobody' ? 'root' : node['dewy']['user']}:#{node['dewy']['group'] == 'nobody' ? 'root' : node['dewy']['group']} dewy
+  CMD
+  only_if { !File.exists?("#{node['dewy']['bin']}/dewy") || node['dewy']['replace'] }
 end
 
 directory node['dewy']['dir'] do
